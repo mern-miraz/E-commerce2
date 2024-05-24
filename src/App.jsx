c820c0fdab47d277a38e0e72981b5a64a85d39f0
Miraz <w3miraj@gmail.com>
2024-05-24 23:52:35 +0600

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Adds from "./components/Adds"
+import Adds2 from "./components/Adds2"
 import Banner from "./components/Banner"
 import Container from "./components/Container"
 import Menu from "./components/Menu"
 import Navbar from "./components/Navbar"
+import NewProduct from "./components/NewProduct"
+import SpecialOffer from "./components/SpecialOffer"
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
     <Menu/>
     <Banner/>
     <Adds/>
+    <NewProduct/>
+    <Adds2/>
+    <SpecialOffer/>
     </>
   )
 }
